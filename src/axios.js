2f349e261589bdb9739cd2e8403a2fc2bddbeb79
Renzo Zapata <110313625+Rezmarr@users.noreply.github.com>
2024-01-16 16:59:47 -0500

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-    baseURL: "https://campground-api20240116001806.azurewebsites.net/api",
+    baseURL: "https://campground-api20240116164010.azurewebsites.net/api",
     withCredentials: true
 });
 
 export const makeRequestPublic = axios.create({
-    baseURL: "https://campground-api20240116001806.azurewebsites.net/api",
+    baseURL: "https://campground-api20240116164010.azurewebsites.net/api",
     // withCredentials: true
 });
