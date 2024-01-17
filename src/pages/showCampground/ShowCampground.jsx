6b743046e4e826647a39d5eebda8fd2546e6b8f8
Renzo Zapata <renzozs2000@gmail.com>
2024-01-17 14:59:02 -0500

    a

--- src/pages/showCampground/ShowCampground.jsx
+++ src/pages/showCampground/ShowCampground.jsx
@@ -32,17 +32,17 @@ function ShowCampground() {
     document.body.classList.remove('hide-scrollbar');
   }
 
-  // const { isLoading, error, data } = useQuery(['campground', id], () =>
-  //   makeRequest.get(`/campground/${id}`).then(res => {
-  //     return res.data;
-  //   })
-  // )
+  const { isLoading, error, data } = useQuery(['campground', id], () =>
+    makeRequest.get(`/campground/${id}`).then(res => {
+      return res.data;
+    })
+  )
 
-  // const { isLoading: reviewsIsLoading, error: reviewsError, data: reviews } = useQuery(['reviews', id], () =>
-  //   makeRequest.get(`/campground/${id}/reviews`).then(res => {
-  //     return res.data;
-  //   })
-  // )
+  const { isLoading: reviewsIsLoading, error: reviewsError, data: reviews } = useQuery(['reviews', id], () =>
+    makeRequest.get(`/campground/${id}/reviews`).then(res => {
+      return res.data;
+    })
+  )
 
   const deleteMutation = useMutation(
     (postId) => {
@@ -61,21 +61,21 @@ function ShowCampground() {
     deleteMutation.mutate(id);
   }
 
-  const data = {
-    id: 1,
-    title: "Camping Río Serpenteante",
-    price: 35,
-    location: "Valle Pintoresco",
-    description: "Acampa junto al río serpenteante y sumérgete en la belleza natural del valle.",
-    picture: "https://cdn.pixabay.com/photo/2021/01/04/10/45/tent-5887144_1280.jpg",
-    user: {
-      firstName: "John",
-      lastName: "Doe"
-    },
-    latitude: "-77.1123622",
-    longitude: "-12.0445345",
-    score: "0"
-  };
+  // const data = {
+  //   id: 1,
+  //   title: "Camping Río Serpenteante",
+  //   price: 35,
+  //   location: "Valle Pintoresco",
+  //   description: "Acampa junto al río serpenteante y sumérgete en la belleza natural del valle.",
+  //   picture: "https://cdn.pixabay.com/photo/2021/01/04/10/45/tent-5887144_1280.jpg",
+  //   user: {
+  //     firstName: "John",
+  //     lastName: "Doe"
+  //   },
+  //   latitude: "-77.1123622",
+  //   longitude: "-12.0445345",
+  //   score: "0"
+  // };
 
   return (
     <div className="showCampground">
