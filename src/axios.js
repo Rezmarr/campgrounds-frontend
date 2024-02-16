4aa3564a4e03a2f7a1a94939db099d2787cbfff2
Renzo Zapata <renzozs2000@gmail.com>
2024-02-16 18:51:15 -0500

    devtunnel

--- src/axios.js
+++ src/axios.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-    baseURL: "https://b19hrc3l-7205.brs.devtunnels.ms//api",
+    baseURL: "https://b19hrc3l-7205.brs.devtunnels.ms/api",
     withCredentials: true
 });
 
 export const makeRequestPublic = axios.create({
-    baseURL: "https://b19hrc3l-7205.brs.devtunnels.ms//api",
+    baseURL: "https://b19hrc3l-7205.brs.devtunnels.ms/api",
     // withCredentials: true
 });
